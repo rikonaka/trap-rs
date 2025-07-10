a86e3f7115d07d851aba699bc2a30bd0e147c5be
RikoNaka <xxy1836@gmail.com>
2025-07-10 17:58:16 +0800

    New codes

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use anyhow::Result;
 use clap::Parser;
 use std::io::ErrorKind;
 use std::io::Read;
+use std::io::Write;
 // use std::io::Write;
 use std::net::IpAddr;
 use std::net::SocketAddr;
@@ -30,6 +31,10 @@ struct Args {
     /// The udp listen port
     #[arg(short, long, default_value = "")]
     udp: String,
+
+    /// When receiving data, return the data set in this parameter
+    #[arg(short, long, default_value = "null", default_missing_value = "", num_args(0..2))]
+    need_return: String,
 }
 
 fn port_parser(input: &str) -> Result<Vec<u16>> {
@@ -55,25 +60,35 @@ fn init_log_level(level: Level) {
     tracing::subscriber::set_global_default(subscriber).expect("failed to set subscriber");
 }
 
-fn handle_client(mut stream: TcpStream) {
+fn handle_client<'a>(mut stream: TcpStream, need_return: String) -> Result<()> {
     let mut buf = vec![0; 4096];
     match stream.read(&mut buf) {
         Ok(recv_size) => {
             if recv_size > 0 {
                 if let Ok(message) = std::str::from_utf8(&buf[0..recv_size]) {
-                    info!("tcp message: [{}]", message.trim_end());
+                    info!("tcp from {}: [{}]", stream.peer_addr()?, message.trim_end());
                 } else {
                     let buf_slice = &buf[0..recv_size];
                     let mut hex_str = String::new();
                     for b in buf_slice {
                         hex_str += &format!("{:02x}", b);
                     }
-                    info!("tcp message: [{}]", hex_str);
+                    info!("tcp from  message: [{}]", hex_str);
                 }
 
-                // if let Err(e) = stream.write_all(&buf[0..recv_size]) {
-                //     eprintln!("send packet failed: {}", e);
-                // }
+                if need_return != "null" {
+                    if need_return.len() > 0 {
+                        match stream.write_all(need_return.as_bytes()) {
+                            Ok(_) => (),
+                            Err(e) => error!("send back data failed: {}", e),
+                        }
+                    } else {
+                        match stream.write_all(&buf[0..recv_size]) {
+                            Ok(_) => (),
+                            Err(e) => error!("send back data failed: {}", e),
+                        }
+                    }
+                }
             } else {
                 warn!("tcp message len is 0");
             }
@@ -84,9 +99,11 @@ fn handle_client(mut stream: TcpStream) {
             }
         }
     }
+    Ok(())
 }
 
-fn tcp_listener(addr: IpAddr, port: u16) -> Result<()> {
+fn tcp_listener(addr: IpAddr, port: u16, need_return: String) -> Result<()> {
+    info!("start tcp listener");
     let socket_addr = SocketAddr::new(addr, port);
     let listener = TcpListener::bind(socket_addr)?;
 
@@ -94,8 +111,9 @@ fn tcp_listener(addr: IpAddr, port: u16) -> Result<()> {
         match stream {
             Ok(stream) => {
                 info!("new client: {}", stream.peer_addr()?);
+                let need_return = need_return.clone();
                 thread::spawn(move || {
-                    handle_client(stream);
+                    handle_client(stream, need_return).expect("handle tcp stream failed");
                 });
             }
             Err(e) => {
@@ -107,7 +125,8 @@ fn tcp_listener(addr: IpAddr, port: u16) -> Result<()> {
     Ok(())
 }
 
-fn udp_listener(addr: IpAddr, port: u16) -> Result<()> {
+fn udp_listener<'a>(addr: IpAddr, port: u16, need_return: String) -> Result<()> {
+    info!("start udp listener");
     let socket_addr = SocketAddr::new(addr, port);
     let socket = UdpSocket::bind(socket_addr)?;
     let mut buf = vec![0; 4096];
@@ -116,18 +135,34 @@ fn udp_listener(addr: IpAddr, port: u16) -> Result<()> {
         match socket.recv_from(&mut buf) {
             Ok((recv_size, src_addr)) => {
                 if recv_size > 0 {
-                    if let Ok(message) = std::str::from_utf8(&buf[0..recv_size]) {
-                        info!("from {}: [{}]", src_addr, message);
-                    } else {
-                        let buf_slice = &buf[0..recv_size];
-                        let mut hex_str = String::new();
-                        for b in buf_slice {
-                            hex_str += &format!("{:02x}", b);
+                    match String::from_utf8(buf[0..recv_size].to_vec()) {
+                        Ok(message) => {
+                            info!("udp from {}: [{}]", src_addr, message.trim_end());
                         }
-                        info!("from {}: [{}]", src_addr, hex_str);
+                        Err(_) => {
+                            let buf_slice = &buf[0..recv_size];
+                            let mut hex_str = String::new();
+                            for b in buf_slice {
+                                hex_str += &format!("{:02x}", b);
+                            }
+                            info!("udp from {}: [{}]", src_addr, hex_str);
+                        }
+                    }
+                    if need_return != "null" {
+                        if need_return.len() > 0 {
+                            match socket.send_to(need_return.as_bytes(), src_addr) {
+                                Ok(_send_size) => (),
+                                Err(e) => error!("send back data failed: {}", e),
+                            };
+                        } else {
+                            match socket.send_to(&buf[0..recv_size], src_addr) {
+                                Ok(_send_size) => (),
+                                Err(e) => error!("send back data failed: {}", e),
+                            };
+                        }
                     }
                 } else {
-                    warn!("from {}, message len is 0", src_addr);
+                    warn!("udp from {}, but data len is 0", src_addr);
                 }
             }
             Err(e) => {
@@ -155,12 +190,14 @@ fn main() {
         .expect(&format!("illegal addr input: {}", args.addr));
 
     let mut handles = Vec::new();
+    let need_return = args.need_return;
     if args.tcp.len() > 0 {
         let tcp_ports =
             port_parser(&args.tcp).expect(&format!("parse tcp port failed: {}", args.tcp));
         for port in tcp_ports {
+            let need_return = need_return.clone();
             let handle = thread::spawn(move || {
-                tcp_listener(addr, port).expect(&format!("start tcp listener failed"));
+                tcp_listener(addr, port, need_return).expect(&format!("start tcp listener failed"));
             });
             handles.push(handle);
         }
@@ -168,8 +205,9 @@ fn main() {
         let udp_ports =
             port_parser(&args.udp).expect(&format!("parse udp port failed: {}", args.tcp));
         for port in udp_ports {
+            let need_return = need_return.clone();
             let handle = thread::spawn(move || {
-                udp_listener(addr, port).expect(&format!("start udp listener failed"));
+                udp_listener(addr, port, need_return).expect(&format!("start udp listener failed"));
             });
             handles.push(handle);
         }
